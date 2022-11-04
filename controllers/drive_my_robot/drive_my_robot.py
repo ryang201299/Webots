6d5af2f851a791c4a664c783306cb607fd980154
ryang201299 <39199732+ryang201299@users.noreply.github.com>
2022-11-04 00:15:21 +0000

    Refactored code
    Refactored code to be more readable and appropriate for future development.
    
    Also added basic comments

--- controllers/drive_my_robot/drive_my_robot.py
+++ controllers/drive_my_robot/drive_my_robot.py
@@ -1,50 +1,93 @@
 from controller import Robot
 
-if __name__ == "__main__":
-    
+TIMESTEP = 64
+MAX_SPEED = 6.1  # 6.28 // also referred to as angular velocity
+NUM_SIDE = 4
+LENGTH_SIDE = 0.25
+WHEEL_RADIUS = 0.025
+DISTANCE_BETWEEN_WHEELS = 0.09
+DEGREES_IN_RADIANS = 6.28 # 360 degrees in radians
+
+def initialise_robot():
+    # Initialise robot object
     robot = Robot()
+
+    # Create motor initialisations
+    left_motor = robot.getDevice('motor_1')
+    right_motor = robot.getDevice('motor_2')
     
-    timestep = 64
-    max_speed = 6.28
-    
-    left_motor = robot.getMotor('motor_1')
-    right_motor = robot.getMotor('motor_2')
-    
+    # Set default motor params
     left_motor.setPosition(float('inf'))
     left_motor.setVelocity(0.0)
     
     right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
     
-    num_side = 4
-    length_side = 0.25
+    return left_motor, right_motor, robot
     
-    wheel_radius = 0.025
-    linear_velocity = wheel_radius * max_speed
-    duration_side = length_side / linear_velocity
-    
+def initialise_environment(robot):
     start_time = robot.getTime()
     
-    angle_of_rotation = 6.28 / num_side
-    distance_between_wheels = 0.090
-    rate_of_rotation = (2 * linear_velocity) / distance_between_wheels
-    duration_turn = angle_of_rotation / rate_of_rotation
+    # Define environment for manuevering square
+    # Vars for straight line
+    linear_velocity = WHEEL_RADIUS * MAX_SPEED # actual speed
+    duration_side = LENGTH_SIDE / linear_velocity
     
+    # Vars for cornering
+    angle_of_rotation = DEGREES_IN_RADIANS / NUM_SIDE
+    rate_of_rotation = (2 * linear_velocity) / DISTANCE_BETWEEN_WHEELS
+    duration_turn = angle_of_rotation / rate_of_rotation
     rot_start_time = start_time + duration_side
     rot_end_time = rot_start_time + duration_turn
     
-    while robot.step(timestep) != -1:
+    return duration_turn, rot_start_time, rot_end_time, duration_side
+    
+def run_time(tools, env_vars):
+    # Separate robot motors from robot
+    left_motor = tools[0]
+    right_motor = tools[1]
+    robot = tools[2]
+    
+    # Environment vars needed for motion
+    duration_turn = env_vars[0]
+    rot_start_time = env_vars[1]
+    rot_end_time = env_vars[2]
+    duration_side = env_vars[3]
+
+    while robot.step(TIMESTEP) != -1:
         current_time = robot.getTime()
         
-        left_speed = max_speed
-        right_speed = max_speed
+        # Set constant speed forward
+        left_speed = MAX_SPEED
+        right_speed = MAX_SPEED
         
+        # Begin rotation at end of side
         if rot_start_time < current_time < rot_end_time:
-            left_speed = -max_speed
-            right_speed = max_speed
+            left_speed = - MAX_SPEED
+            right_speed = MAX_SPEED
+         
+        # End rotation at end of corner
         elif current_time > rot_end_time:
             rot_start_time = current_time + duration_side
-            rot_end_time = rot_start_time + duration_turn
-        
+            rot_end_time = rot_start_time + duration_turn    
+                    
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
+    
+def main():
+    # Initialise robot
+    # Tools contains our robot, and left and right motors
+    tools = initialise_robot()
+    robot = tools[2]
+   
+    # Initialise environment
+    # env_vars contains basic environment measurements like
+    # length of sides, determined speed, rotation angles etc.
+    env_vars = initialise_environment(robot)
+    
+    # Run environment
+    run_time(tools, env_vars)
+    
+if __name__ == "__main__":
+    main()
+    
